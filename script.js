d71a04a8c40189a2d964f58d54d4145ae2ea2d8b
JINLEI LIN <xzorkz@gmail.com>
2024-05-23 15:32:32 +0200

    removed 1st line and fixed 100 EUR / 1 h writting

--- script.js
+++ script.js
@@ -79,7 +79,6 @@ function generateOfferText(busSeat, startCity, startDate, endCity, endDate, delt
 
 
     offerText = `
-Thank you for your kind request for coach service by an Apollo coaches.
 
 Please find our best price and conditions below.
 
@@ -92,7 +91,7 @@ Service description: ${startCity} - ${endCity}
 Price: ${price}€
 ${inclusions}
 
-Offer is valid for 12h service per day. Extra hours will be charged 100EURO/1 extra hour.
+Offer is valid for 12h service per day. Extra hours will be charged 100 EUR/1 extra hour.
 
 This offer is provided for informational purposes only; no coach has been booked at this time.
 
